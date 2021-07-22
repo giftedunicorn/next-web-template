40147b7f8abdc86793d90db2438949c0ea8a41c6
ednew2018 <shenjian8628@gmail.com>
2021-07-22 14:02:18 -0700

    Add NextSEO component

---
+++ components/NextSEO.js
@@ -0,0 +1,12 @@
+// import { withTranslation } from 'utils/i18n'
+// import { useRouter } from 'next/router'
+// import SEO from "utils/seo"; 
+// import { NextSeo } from 'next-seo';
+
+const NextSEO = (props) => {
+  return (
+    <div>SEO</div>
+  )
+}
+
+export default NextSEO;
