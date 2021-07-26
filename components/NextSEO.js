9091b387382cf49c05742dfaf618d1a144687c0e
ednew2018 <shenjian8628@gmail.com>
2021-07-25 21:24:40 -0700

    Update next seo and add zh-cn to seo

--- components/NextSEO.js
+++ components/NextSEO.js
@@ -1,11 +1,12 @@
-// import { withTranslation } from 'utils/i18n'
+import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router'
 import { NextSeo } from 'next-seo';
 import SEO from "../utils/seo"; 
 
-function NextSEO(props) {
+const NextSEO = (props) => {
+  const { i18n } = useTranslation()
   const router = useRouter()
-  let language // let { language } = props.i18n
+  let { language } = i18n
   let { asPath } = router
   language = language || 'en'
   // use asPath instead of route for /download/[device]
@@ -13,8 +14,8 @@ function NextSEO(props) {
   
   // remove trail slash
   let pathname = asPath
+  // pathname = pathname.replace('/zh-CN', '')
   // pathname = pathname.replace('/zh-TW', '')
-  // pathname = pathname.replace('/zh', '')
   if (pathname === '/') pathname = ''
 
   const seo = SEO[language][asPath] || SEO.en['/']
@@ -53,7 +54,7 @@ function NextSEO(props) {
         },
         {
           hrefLang: 'zh-CN',
-          href: `https://example.com/zh${pathname}`,
+          href: `https://example.com/zh-CN${pathname}`,
         },
         {
           hrefLang: 'zh-TW',
@@ -64,5 +65,4 @@ function NextSEO(props) {
   )
 }
 
-// export default withTranslation('common')(NextSEO);
 export default NextSEO;
---
+++ utils/seo/zh-CN.js
@@ -0,0 +1,9 @@
+export default {
+  "/": {
+    title: "网站title",
+    description: "网站description",
+    url: "https://mysite.com",
+    image: "https://mysite.com/image.jpg",
+    locale: "zh-CN",
+  }
+}
