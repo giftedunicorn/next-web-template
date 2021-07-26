33c6d220cee985ad4a12fd24371f2edefd714846
ednew2018 <shenjian8628@gmail.com>
2021-07-25 22:54:08 -0700

    Add axios and update apis

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@ import * as log from 'loglevel';
 import axios from 'axios';
 axios.defaults.timeout = 30000;
 
-async function axiosHelper({ method, url, name, body, auth }) {
+async function axiosHelper({ method, url, name, body }) {
   if (name) log.debug(`${name} invoked`);
 
   try {
@@ -12,30 +12,36 @@ async function axiosHelper({ method, url, name, body, auth }) {
       data: body,
     }
 
-    if (auth) {
-      const token = await getIdTokenHelper()
-      if (token) {
-        options.headers = {
-          "Authorization": token,
-        }
-      }
-    }
-
     const response = await axios(options)
-    const {data} = response;
+    const { data } = response;
+
     if (data.statusCode !== 200) {
       if (data.message) throw new Error(`${name} ERROR, ${data.message}`)
-      else throw new Error(`${name} ERROR`)
+      else throw new Error(`${name} ERROR, ${data.statusCode}`)
     } else {
-      log.debug(`${name} RESULT`);
+      log.debug(`${name} DONE`);
       return data
     }
   } catch (error) {
-    log.error(`${name} ERROR`, error)
-    if (error && error.response && error.response.data && error.response.data.message) throw new Error(error.response.data.message)
-    else if (error && error.response && error.response.statusText) throw new Error(error.response.statusText)
-    else if (error && error.message) throw new Error(error.message)
-    else throw error
+    log.error(`${name} ERROR`)
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      log.error(error.response.data);
+      log.error(error.response.status);
+      log.error(error.response.headers);
+      throw new Error(error.message)
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      log.error(error.request);
+      throw new Error(error.message)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      log.error('Error', error.message);
+      throw new Error(error.message)
+    }
   }
 }
 
